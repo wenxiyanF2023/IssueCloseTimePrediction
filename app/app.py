a32ce54b7a0b0ff27fde698dc9e800aad967c3c5
wenxiyanF2023 <wenxiyan@andrew.cmu.edu>
2024-04-09 23:30:50 -0700

    allow download of model during deployment

--- app/app.py
+++ app/app.py
@@ -12,7 +12,22 @@ import ssl
 from transformers import BertTokenizer, BertModel
 import torch
 from flask_cors import CORS
+import os
 
+MODEL_URL = 'https://drive.google.com/file/d/1yV2ruB2b6L6Y1lnY_E_daDCPBxntrDuZ/view?usp=sharing'
+MODEL_PATH = 'random_forest_model.joblib'
+
+def download_model(url, path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(path, 'wb') as f:
+            f.write(response.content)
+    else:
+        raise Exception(f"Failed to download model: {response.status_code}")
+
+# Download model if it doesn't exist
+if not os.path.exists(MODEL_PATH):
+    download_model(MODEL_URL, MODEL_PATH)
 
 app = Flask(__name__)
 CORS(app)
